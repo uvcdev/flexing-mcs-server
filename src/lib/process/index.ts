f2bf4feb9c22258e06d885c9ddca9405dd339cfd
ljk <ljk@uvc.co.kr>
2025-04-23 14:46:59 +0900

    [FIX] 주석 해제

--- src/lib/process/index.ts
+++ src/lib/process/index.ts
@@ -19,7 +19,7 @@ const heapUse = () => {
   const heapUsedMB = (memoryUsage.heapUsed / 1024 / 1024).toFixed(2);
   const heapTotalMB = (memoryUsage.heapTotal / 1024 / 1024).toFixed(2);
 
-  // console.log(`heap use: ${heapUsedMB} MB / ${heapTotalMB} MB`);
+  console.log(`heap use: ${heapUsedMB} MB / ${heapTotalMB} MB`);
 }
 let counter = 0;
 export const processMcs = async () => {
@@ -49,7 +49,7 @@ export const processMcs = async () => {
     await checkBranchInfoReqForWms()
 
     // ACK_CALL_INFO 판단해서 콜 정보 저장과 EQP에 응답 데이터 Write
-    // await useCallRegisterUtil().checkCallSave()
+    await useCallRegisterUtil().checkCallSave()
     // todo4: 창고로부터 ACK 오면 EQP_Call_Save 함수와 같은 기능 실행
 
 
