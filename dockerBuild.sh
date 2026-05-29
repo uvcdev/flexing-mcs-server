b952860d24fbae8f266513aec1004f77e31acd4c
leesangkwon-dev <lsk@uvc.co.kr>
2026-05-29 10:11:49 +0900

    [UPDATE] version 2.2.18

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOCKER_USER_NAME=uvclab
 DOCKER_IMAGE_NAME=flexing-mcs-server
-DOCKER_IMAGE_TAG=2.2.17
+DOCKER_IMAGE_TAG=2.2.18
 SAVE_DIR=../images
 
 docker build -t $DOCKER_USER_NAME/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
