8c125ccc4d2ca1584736e89e457ba6599cfc9844
ljk <ljk@uvc.co.kr>
2026-06-17 14:32:35 +0900

    [FIX] Kepserver 접속 무한 재시도 변경

--- src/config/kepserverConfig.ts
+++ src/config/kepserverConfig.ts
@@ -1,24 +1,21 @@
-import { MessageSecurityMode, SecurityPolicy } from "node-opcua-client";
-import { createClientCertificate } from "../service/kepware/certificateService";
-import dotenv from "dotenv"
-
+import { MessageSecurityMode, SecurityPolicy } from 'node-opcua-client';
+import { createClientCertificate } from '../service/kepware/certificateService';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-let certificateFile = "";
-let privateKeyFile = "";
+let certificateFile = '';
+let privateKeyFile = '';
 
 void (async () => {
   const { clientCertificateFile, clientPrivateKeyFile } = await createClientCertificate();
 
   certificateFile = clientCertificateFile;
   privateKeyFile = clientPrivateKeyFile;
-
 })();
 
 export const kepserverConfig = {
-
-  endpointUrl: process.env.OPCUA_ENDPOINT || "opc.tcp://localhost:49320",
+  endpointUrl: process.env.OPCUA_ENDPOINT || 'opc.tcp://localhost:49320',
 
   clientOptions: {
     // applicationName: "MyOPCUAClient",
@@ -35,21 +32,19 @@ export const kepserverConfig = {
 
     endpointMustExist: false,
     connectionStrategy: {
-      maxRetry: 2,            // 최대 5번 재시도
-      initialDelay: 1000,      // 초기 지연 1초
-      maxDelay: 10000,         // 최대 지연 10초
-      randomisationFactor: 0.2 // 랜덤성 추가
+      maxRetry: -1, // 최대 5번 재시도 -> 무한 재시도
+      initialDelay: 1000, // 초기 지연 1초
+      maxDelay: 10000, // 최대 지연 10초
+      randomisationFactor: 0.2, // 랜덤성 추가
     },
   },
 
   subscribeOptions: {
-    requestedPublishingInterval: 1000,      // 1초 간격으로 데이터 전송 => 2000
-    requestedLifetimeCount: 300,			// 300초 유효 시간 1000ms * 300  => 500
-    requestedMaxKeepAliveCount: 20,			// 20초 간격으로 KeepAlive 메세지 1000ms * 20
-    maxNotificationsPerPublish: 1000,		// 한 번에 최대 1000개의 알림  => 500
+    requestedPublishingInterval: 1000, // 1초 간격으로 데이터 전송 => 2000
+    requestedLifetimeCount: 300, // 300초 유효 시간 1000ms * 300  => 500
+    requestedMaxKeepAliveCount: 20, // 20초 간격으로 KeepAlive 메세지 1000ms * 20
+    maxNotificationsPerPublish: 1000, // 한 번에 최대 1000개의 알림  => 500
     publishingEnabled: true,
     priority: 10,
-  }
-
+  },
 };
-
